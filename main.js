8cad310ecbdb4b16a059343959aa4e9db65dd0f7
hahasumin <hahasuminn@gmail.com>
2021-09-29 20:46:37 +1000

    minishopping completed

--- main.js
+++ main.js
@@ -10,8 +10,6 @@ function loadItems() {
 // Update the list with the given items
 function displayItems(items) {
   const container = document.querySelector(".items");
-  const html = items.map((item) => createHTMLString(item)).join("");
-  console.log(html);
   container.innerHTML = items.map((item) => createHTMLString(item)).join("");
 }
 
@@ -24,10 +22,36 @@ function createHTMLString(item) {
   </li>
   `;
 }
+
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+
+  // const filtered = items.filter((item) => item[key] === value);
+  // console.log(filtered);
+
+  displayItems(items.filter((item) => item[key] == value));
+
+  // console.log(event.target.dataset.key);
+  // console.log(event.target.dataset.value);
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector(".logo");
+  const buttons = document.querySelector(".buttons");
+  logo.addEventListener("click", () => displayItems(items));
+  buttons.addEventListener("click", (event) => onButtonClick(event, items));
+}
+
 // main
 loadItems()
   .then((items) => {
     displayItems(items);
-    // setEventListeners(items);
+    setEventListeners(items);
   })
   .catch(console.log);
